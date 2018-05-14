e83a36e7698f00e0e76525a6ab59f6cfb29cfcb3
Rachel Park <mjpark03@gmail.com>
2018-05-14 18:43:42 +0900

    jewels-and-stones-1 for Java

---
+++ jewels-and-stones-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+      
+        for (char ch: S.toCharArray()) {
+            if (J.indexOf(ch) != -1)
+                count++;
+        }
+        
+        return count;
+    }
+}
